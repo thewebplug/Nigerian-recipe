570c612666e611bc98f9dfa1d13c0dd38c9a258e
Saleem Jibril <saleemjibril5@gmail.com>
2023-08-29 10:59:10 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,16 @@
+
 import logo from "./logo.svg";
 import "./App.css";
 import Image from "./img/Groundnut-Soup.-Photo-The-Kitchen-Muse-e1599948546462.jpg";
+import data from "./data";
+
+
+
+
+
 
 function App() {
 
-  const data = {
-    name: "My Groundnut soup recipe",
-    image: "https://guardian.ng/wp-content/uploads/2020/09/Groundnut-Soup.-Photo-The-Kitchen-Muse-e1599948546462.jpg",
-    ingredients: [
-        "1 cup, Groundnut (roasted or raw)",
-        "Assorted meats",
-        "Chopped onions",
-        "1 medium-size dried stockfish",
-        "Fresh pepper",
-        "Palm oil",
-        "Crayfish",
-        "1/2 Bitter leaves",
-        "1/4 cup, Uziza seeds",
-        "1/4 Efinrin /Basil leaves (optional)"
-    ],
-    instructions: [
-        "In a medium-sized pot, Season and boil assorted meat with chopped onions, salt and seasoning.",
-        "Dry roast the groundnut and uziza seed for 3 minutes in a frying pan shaking the pan regularly. I find that roasting spices and nut in a pan release oils and intensifies the flavour.",
-        "Blend the groundnut and the uziza seeds until you achieve finely textured powder and set aside. The longer you blend, you will notice the powder transforming into a paste. This is as a result of the oils in the nuts and seeds. Roughly blend the pepper and set aside.",
-        "When the meat is tender, shred stockfish into bits and add.",
-        "Add the pepper in the assorted meat stir and let it boil properly. This should take 3 minutes, taste and add salt and seasoning if necessary. It is now time to include a reasonable amount of palm oil. I prefer 2 spoons of palm oil.",
-        "Add the groundnut powder and 1/2 soup hot water. Note that the Palm oil and pepper stock must be bubbly while adding the groundnut powder.",
-        "Let it boil for 10 minutes and watch as the soup thickens. If you are left with a watery consistency, simply add extra powder.",
-        "Add a handful of efinrin, stir let it this cook for 2 minutes under low heat stir again.",
-        "Serve with pounded yam /garri/fufu/wheat."
-    ],
-    notes: [
-      "Groundnuts are naturally cholesterol-free. They’re a great plant-based option, containing more protein than any nut and several essential vitamins and minerals as well.",
-      "Groundnuts are the most sustainable nut. From the roots to the hulls, no part of the plant goes to waste. They also have the most efficient water use carbon footprint."
-    ]
-}
 
   return (
     <div className="">
